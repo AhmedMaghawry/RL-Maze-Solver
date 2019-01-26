313459d4bbc43358d8eb20c99165c3f3f474ac2a
Ahmed Ezzat <a.maghawry25@gmail.com>
2019-01-26 20:49:28 +0200

    Add files via upload

--- grid.py
+++ grid.py
@@ -1,10 +1,4 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 import numpy as np
-import matplotlib.pyplot as plt
-import cv2
-
 
 class MazeMDP:
 
@@ -23,15 +17,20 @@ class MazeMDP:
                  action_probabilities,
                  no_action_probability):
 
-        self._reward_grid = reward_grid
+        
         self._terminal_flag = terminal_flag
         self._obstacle_flag = obstacle_flag
-        self._T = self._create_transition_matrix(
+        self._reward_grid = reward_grid
+        self._T = self._calculate_transition_matrix(
             action_probabilities,
             no_action_probability,
             obstacle_flag
         )
-
+        
+    @property
+    def reward_grid(self):
+        return self._reward_grid
+    
     @property
     def shape(self):
         return self._reward_grid.shape
@@ -40,31 +39,38 @@ class MazeMDP:
     def size(self):
         return self._reward_grid.size
 
-    @property
-    def reward_grid(self):
-        return self._reward_grid
+ 
 
-    def value_evalution(self, discount=1.0,
-                             iterations=10):
-        utility_grids, policy_grids = self._init_utility_policy_storage(iterations)
+   
+    def _policy_iteration(self, *, utility_grid,
+                          policy_grid, discount=1.0):
+        # policy iteration algorthim
+        r, c = self.from_indices_to_coordinates()
 
-        utility_grid = np.zeros_like(self._reward_grid)
-        for i in range(iterations):
-            utility_grid = self._value_iteration(utility_grid=utility_grid)
-            policy_grids[:, :, i] = self.best_policy(utility_grid)
-            utility_grids[:, :, i] = utility_grid
-        return policy_grids, utility_grids
+        M, N = self.shape
 
+        utility_grid = (
+            self._reward_grid +
+            discount * ((utility_grid.reshape((1, 1, 1, M, N)) * self._T)
+                        .sum(axis=-1).sum(axis=-1))[r, c, policy_grid.flatten()]
+            .reshape(self.shape)
+        )
+
+        utility_grid[self._terminal_flag] = self._reward_grid[self._terminal_flag]
+
+        return self.optimal_policy(utility_grid), utility_grid
+    
     def policy_evalution(self, discount=1.0,
                               iterations=10):
-        utility_grids, policy_grids = self._init_utility_policy_storage(iterations)
+        # perform policy evalution multiple times
+        utility_grids, policy_grids = self._init_ans(iterations)
 
         policy_grid = np.random.randint(0, self._num_actions,
                                         self.shape)
         utility_grid = self._reward_grid.copy()
 
         for i in range(iterations):
-            policy_grid, utility_grid = self._policy_evalution(
+            policy_grid, utility_grid = self._policy_iteration(
                 policy_grid=policy_grid,
                 utility_grid=utility_grid
             )
@@ -73,38 +79,58 @@ class MazeMDP:
         return policy_grids, utility_grids
 
 
-    def grid_indices_to_coordinates(self, indices=None):
+    def from_indices_to_coordinates(self, indices=None):
         if indices is None:
             indices = np.arange(self.size)
         return np.unravel_index(indices, self.shape)
-
-    def grid_coordinates_to_indices(self, coordinates=None):
-        # Annoyingly, this doesn't work for negative indices.
-        # The mode='wrap' parameter only works on positive indices.
-        if coordinates is None:
-            return np.arange(self.size)
-        return np.ravel_multi_index(coordinates, self.shape)
-
-    def best_policy(self, utility_grid):
+    
+    def _value_iteration(self, utility_grid, discount=1.0):
+        # value iteration algorthim
+        out = np.zeros_like(utility_grid)
+        M, N = self.shape
+        for i in range(M):
+            for j in range(N):
+                out[i, j] = self._calculate_utility((i, j),
+                                                    discount,
+                                                    utility_grid)
+        return out
+    
+    def value_evalution(self, discount=1.0,
+                             iterations=10):
+        # perform value iteration multiple times
+           utility_grids, policy_grids = self._init_ans(iterations)
+    
+           utility_grid = np.zeros_like(self._reward_grid)
+           for i in range(iterations):
+               utility_grid = self._value_iteration(utility_grid=utility_grid)
+               policy_grids[:, :, i] = self.optimal_policy(utility_grid)
+               utility_grids[:, :, i] = utility_grid
+           return policy_grids, utility_grids
+       
+        
+    def optimal_policy(self, utility_grid):
+        # compute the best policy by maximizing utility
         M, N = self.shape
         return np.argmax((utility_grid.reshape((1, 1, 1, M, N)) * self._T)
                          .sum(axis=-1).sum(axis=-1), axis=2)
 
-    def _init_utility_policy_storage(self, depth):
+    def _init_ans(self, depth):
+        #intialize utitlty and policy grids
         M, N = self.shape
         utility_grids = np.zeros((M, N, depth))
         policy_grids = np.zeros_like(utility_grids)
         return utility_grids, policy_grids
 
-    def _create_transition_matrix(self,
+    def _calculate_transition_matrix(self,
                                   action_probabilities,
                                   no_action_probability,
                                   obstacle_flag):
+        ## compute transition matrix
         M, N = self.shape
 
         T = np.zeros((M, N, self._num_actions, M, N))
 
-        r0, c0 = self.grid_indices_to_coordinates()
+        r0, c0 = self.from_indices_to_coordinates()
 
         T[r0, c0, :, r0, c0] += no_action_probability
 
@@ -125,33 +151,6 @@ class MazeMDP:
         terminal_locs = np.where(self._terminal_flag.flatten())[0]
         T[r0[terminal_locs], c0[terminal_locs], :, :, :] = 0
         return T
-
-    def _value_iteration(self, utility_grid, discount=1.0):
-        out = np.zeros_like(utility_grid)
-        M, N = self.shape
-        for i in range(M):
-            for j in range(N):
-                out[i, j] = self._calculate_utility((i, j),
-                                                    discount,
-                                                    utility_grid)
-        return out
-
-    def _policy_evalution(self, *, utility_grid,
-                          policy_grid, discount=1.0):
-        r, c = self.grid_indices_to_coordinates()
-
-        M, N = self.shape
-
-        utility_grid = (
-            self._reward_grid +
-            discount * ((utility_grid.reshape((1, 1, 1, M, N)) * self._T)
-                        .sum(axis=-1).sum(axis=-1))[r, c, policy_grid.flatten()]
-            .reshape(self.shape)
-        )
-
-        utility_grid[self._terminal_flag] = self._reward_grid[self._terminal_flag]
-
-        return self.best_policy(utility_grid), utility_grid
 
     def _calculate_utility(self, loc, discount, utility_grid):
         if self._terminal_flag[loc]:
